5ef607b032f8334ff27a5f56a9987207968f7e81
geckotang <macky1207@gmail.com>
2014-12-04 21:46:10 +0900

    右上の追加ボタン対応
    content-scriptだとwindowオブジェクトにアクセス出来ないので、
    ページにスクリプトを埋め込んでそこから操作するようにした。
    イベントを発火させるために、Prototype.jsのプラグインを追加している。

--- common.js
+++ common.js
@@ -1,13 +1,53 @@
-(function($){
+(function($, p$, p$$){
+  var extensionID = 'backlog_attachment_alert';
+
+
+
+
   var contentArea = $('#bodyLeft');
   var submitButton = $('input[type="submit"], .submitbtn', contentArea);
   var addTicketForm = $('#AddIssueConfirm');
 
+
+
+
+  var $dummyDirect = $('<input class="submitbtn Btn-gray" value="★追加★" type="button"/>');
+  var $direct = $('.submitbtn.direct');
+  var directID = extensionID + '_direct';
+  $dummyDirect.on('click', function(){
+    if (checkAddFiles($(this))) {
+      p$(directID).simulate('click');
+    }
+  });
+  $direct.after($dummyDirect);
+  $direct.attr('id', directID);
+  $direct.hide();
+
+
+
+
   submitButton.on('click', function(){
-    var keyword = $(this).siblings().find('textarea').val();
-    if(keyword.indexOf('添付') !== -1){
-      addTicketForm.off('submit');
+    if ($(this).is('.direct')) {
+      return;
+    }
+    return checkAddFiles($(this));
+  });
+
+
+
+
+  function checkAddFiles($btn){
+    var $textarea = $btn.closest('form[enctype="multipart/form-data"]').find('textarea');
+    var keyword = '';
+    $textarea.each(function(){
+      keyword += $(this).val();
+    });
+    if(/添付/.test(keyword)){
       return confirm('添付漏れはありませんか？');
     }
-  })
-})(jQuery);
+  };
+
+
+
+
+})(jQuery, $, $$);
