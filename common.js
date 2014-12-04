c596e0a9e66ab5f0db89b258e0aea5b8711b549f
geckotang <macky1207@gmail.com>
2014-12-04 22:22:45 +0900

    よく使うものを変数にまとめた

--- common.js
+++ common.js
@@ -2,27 +2,36 @@
   var contentArea = $('#bodyLeft');
   var addTicketForm = $('#AddIssueConfirm');
   var addTickectButton = $('input[type="button"].submitbtn', contentArea);
+
+  var confirm_msg = '添付漏れはありませんか？';
+  var reg_textarea = /添付/;
+  var cls_direct = 'direct';
+  var cls_clone = 'clone';
+  var selector_direct = '.'+cls_direct;
+
   addTickectButton.each(function(){
-    $(this).clone().removeClass('direct').addClass('clone').insertAfter($(this));
-    $(this).hide();
+    var $this = $(this);
+    $this.clone().removeClass(cls_direct).addClass(cls_clone).insertAfter($this);
+    $this.hide();
   });
-  var submitButton = $('input[type="submit"], input.clone', contentArea);
+  var submitButton = $('input[type="submit"]' + ', input.'+cls_clone , contentArea);
 
   submitButton.on('click', function(){
-    var keyword = $(this).closest('form[enctype="multipart/form-data"]').find('textarea').val();
-    if(/添付/.test(keyword)){
-      submitFlag = confirm('添付漏れはありませんか？');
+    var $this = $(this);
+    var keyword = $this.closest('form[enctype="multipart/form-data"]').find('textarea').val();
+    if(reg_textarea.test(keyword)){
+      submitFlag = confirm(confirm_msg);
       if(submitFlag){
-        if($(this).hasClass('clone')){
-          $(this).prev('.direct').trigger('click');
+        if($this.hasClass(cls_clone)){
+          $this.prev(selector_direct).trigger('click');
         }
         return true;
       }else{
         return false;
       }
     }else{
-      if($(this).hasClass('clone')){
-        $(this).prev('.direct').trigger('click');
+      if($this.hasClass(cls_clone)){
+        $this.prev(selector_direct).trigger('click');
       }
       return true;
     }
